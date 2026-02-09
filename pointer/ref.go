9a3528207ecefae2053c2e29cdeac0fe5a3ad566
Bigsk <i@ianxia.com>
2026-02-09 21:25:45 +0800

    Added SliceRef

--- pointer/ref.go
+++ pointer/ref.go
@@ -4,3 +4,22 @@ package pointer
 func Ref[T any](v T) *T {
 	return &v
 }
+
+// SliceRef returns a slice of pointers to the given slice
+func SliceRef[T any](v []T) []*T {
+	ptrs := make([]*T, len(v))
+	for i := range v {
+		ptrs[i] = &v[i]
+	}
+	return ptrs
+}
+
+// SliceCopyRef returns a slice of pointers to a copy of the given slice
+func SliceCopyRef[T any](v []T) []*T {
+	ptrs := make([]*T, len(v))
+	for i := range v {
+		vcpy := v[i]
+		ptrs[i] = &vcpy
+	}
+	return ptrs
+}
